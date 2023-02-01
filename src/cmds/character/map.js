e9e76354a011c2470ae1b84b8177d9894a90e9db
Eman134 <kaykeeman@gmail.com>
2023-01-31 22:52:51 -0300

    Check water in canvas and routes

--- src/cmds/character/map.js
+++ src/cmds/character/map.js
@@ -1,6 +1,8 @@
 const BaseCommand = require('../../utils/BaseCommand')
 const Canvas = require('canvas')
 const { AttachmentBuilder } = require('discord.js')
+const randomNumber = require('../../utils/randomNumber')
+const isWater = require('../../utils/isWater')
 
 module.exports = class Command extends BaseCommand {
 	constructor(client) {
@@ -16,13 +18,22 @@ module.exports = class Command extends BaseCommand {
 
 		const tileWidth = 1879
 		const tileHeight = 934
-
 		const scale = 0.6
 		const playerWidth = tileWidth * scale
 		const playerHeight = tileHeight * scale
+		const totalWidth = tileWidth * 10
+		const totalHeight = tileHeight * 10
+
+		const isRouting = true
+
+		const routingToPosition = {
+			x: randomNumber(0, totalWidth),
+			y: randomNumber(0, totalHeight),
+		}
+
 		const playerPosition = {
-			x: 10700,
-			y: 3600,
+			x: randomNumber(0, totalWidth),
+			y: randomNumber(0, totalHeight),
 		}
 
 		const playerPositionTile = {
@@ -30,7 +41,16 @@ module.exports = class Command extends BaseCommand {
 			y: (playerPosition.y % tileHeight) * scale,
 		}
 
+		const routingToPositionTileDirection = {
+			x: (routingToPosition.x % tileWidth) * scale,
+			y: (routingToPosition.y % tileHeight) * scale,
+		}
+
+		const routingAngle = (Math.atan2(routingToPositionTileDirection.y - playerPositionTile.y, routingToPositionTileDirection.x - playerPositionTile.x) * 180 / Math.PI).toFixed(2)
+
 		const tile = (Math.floor(playerPosition.x / tileWidth) + 1) + (Math.floor(playerPosition.y / tileHeight) * 10)
+
+		const routingTile = (Math.floor(routingToPosition.x / tileWidth) + 1) + (Math.floor(routingToPosition.y / tileHeight) * 10)
 
 		const canvas = Canvas.createCanvas(playerWidth, playerHeight)
 
@@ -43,6 +63,15 @@ module.exports = class Command extends BaseCommand {
 		const image = await Canvas.loadImage(Buffer.from(tileData.data, 'base64'))
 
 		ctx.drawImage(image, 0, 0, playerWidth, playerHeight)
+
+		const checkArea = 50
+
+		const imageCheckAreaData = ctx.getImageData(playerPositionTile.x - checkArea, playerPositionTile.y - checkArea, checkArea * 2, checkArea * 2)
+
+		const checkAreaCanvas = Canvas.createCanvas(checkArea * 2, checkArea * 2)
+		const checkAreaCtx = checkAreaCanvas.getContext('2d')
+		checkAreaCtx.putImageData(imageCheckAreaData, 0, 0)
+
 		ctx.fillStyle = '#FF0000'
 		ctx.fillRect(playerPositionTile.x, playerPositionTile.y, 10, 10)
 
@@ -52,8 +81,39 @@ module.exports = class Command extends BaseCommand {
 		ctx.strokeStyle = '#FF0000'
 		ctx.stroke()
 
-		const attachment = new AttachmentBuilder(canvas.toBuffer(), { name: 'map.png' })
+		ctx.font = '20px Arial'
+		ctx.fillStyle = '#FFFFFF'
+		ctx.fillText(`X: ${playerPosition.x}, Y: ${playerPosition.y}`, playerPositionTile.x + 10, playerPositionTile.y - 10)
 
-		interaction.editReply({ files: [attachment] })
+		const { waterPercentage } = isWater(checkAreaCanvas, checkAreaCtx)
+
+		const inWater = waterPercentage > 0.6
+
+		if (inWater) {
+			ctx.beginPath()
+			ctx.arc(playerPositionTile.x + 5, playerPositionTile.y + 5, 60, 0, Math.PI * 2, true)
+			ctx.closePath()
+			ctx.strokeStyle = '#00FF00'
+			ctx.stroke()
+		}
+
+		if (inWater) {
+			ctx.fillText('🌊', playerPositionTile.x + 10, playerPositionTile.y + 20)
+		}
+
+		if (isRouting) {
+			ctx.beginPath()
+			ctx.moveTo(playerPositionTile.x + 5, playerPositionTile.y + 5)
+			ctx.lineTo(playerPositionTile.x + 5 + (Math.cos(routingAngle * Math.PI / 180) * 1000), playerPositionTile.y + 5 + (Math.sin(routingAngle * Math.PI / 180) * 1000))
+			ctx.closePath()
+			ctx.strokeStyle = '#FF00FF'
+			ctx.lineWidth = 5
+			ctx.setLineDash([3, 15])
+			ctx.stroke()
+		}
+
+		const attachment = new AttachmentBuilder(canvas.toBuffer(), { name: 'map.png' })
+		const isRoutingMessage = isRouting ? `Rota para X: ${routingToPosition.x}, Y: ${routingToPosition.y}, Tile: ${routingTile}, Angulo: ${routingAngle}` : ''
+		interaction.editReply({ content: `[X: ${playerPosition.x}, Y: ${playerPosition.y}, Tile: ${tile}]\n${isRoutingMessage}\nEm água: ${inWater} (${waterPercentage * 100}%)`, files: [attachment] })
 	}
 }
---
+++ src/utils/isWater.js
@@ -0,0 +1,108 @@
+module.exports = (checkAreaCanvas, checkAreaCtx) => {
+	let waterInArea = 0
+	let totalInArea = 0
+
+	const waterColors = [
+		[54, 152, 155],
+		[124, 153, 196],
+		[139, 165, 203],
+		[150, 170, 201],
+		[107, 139, 188],
+		[109, 134, 174],
+		[128, 154, 193],
+		[212, 212, 212],
+		[123, 128, 137],
+		[126, 154, 197],
+		[130, 146, 169],
+		[124, 152, 195],
+		[105, 137, 186],
+		[106, 138, 187],
+		[104, 136, 185],
+		[122, 151, 194],
+		[123, 152, 195],
+		[138, 164, 202],
+		[137, 163, 201],
+		[103, 135, 184],
+		[107, 139, 187],
+		[120, 149, 192],
+		[152, 175, 209],
+		[151, 174, 208],
+		[150, 173, 207],
+		[149, 172, 206],
+		[142, 163, 195],
+		[147, 169, 202],
+		[135, 161, 199],
+	]
+
+	const notWaterColors = [
+		[0, 0, 0],
+		[183, 207, 217],
+		[255, 255, 255],
+		[117, 123, 133],
+		[116, 123, 133],
+	]
+
+	const colors = {}
+	const usedColors = []
+	const waterColorsUsed = []
+
+	const isSimilarColor = (color1, color2, tolerance) => {
+		const distance = Math.sqrt(
+			Math.pow(color1[0] - color2[0], 2) +
+			Math.pow(color1[1] - color2[1], 2) +
+			Math.pow(color1[2] - color2[2], 2),
+		)
+		return distance <= tolerance
+	}
+
+	for (let x = 0; x < checkAreaCanvas.width; x++) {
+		for (let y = 0; y < checkAreaCanvas.height; y++) {
+			const pixel = checkAreaCtx.getImageData(x, y, 1, 1).data
+			const color = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`
+			if (notWaterColors.some(notWaterColor => notWaterColor[0] === pixel[0] && notWaterColor[1] === pixel[1] && notWaterColor[2] === pixel[2])) {
+				continue
+			}
+			if (colors[color]) {
+				colors[color]++
+			}
+			else {
+				colors[color] = 1
+			}
+
+			let isWater = false
+
+			for (const waterColor of waterColors) {
+				if (isSimilarColor(pixel, waterColor, 3)) {
+					isWater = true
+					break
+				}
+			}
+
+			if (isWater) {
+				waterInArea++
+				if (!waterColorsUsed.includes(color)) {
+					waterColorsUsed.push(color)
+				}
+			}
+			totalInArea++
+
+			if (!usedColors.includes(color)) {
+				usedColors.push(color)
+			}
+
+		}
+	}
+
+	const waterPercentage = (waterInArea / totalInArea).toFixed(4)
+
+	if (waterPercentage > 0.4) {
+		const notUsedColors = usedColors.filter(color => !waterColorsUsed.includes(color) && colors[color] / totalInArea > 0.01)
+		if (notUsedColors.length > 0) {
+			console.log(notUsedColors.map(color => `CheckWater: ${color}: ${colors[color]} (${(colors[color] / totalInArea * 100).toFixed(2)}%)`))
+		}
+	}
+
+	return {
+		waterPercentage,
+	}
+}
